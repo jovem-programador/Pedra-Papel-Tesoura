b201f3acff426cf32f6b1cbf4ae2c740158e48af
Anderson Marley <51322494+jovem-programador@users.noreply.github.com>
2023-06-12 22:48:25 -0300

    Codigo refatorado

--- codigo/script.js
+++ codigo/script.js
@@ -17,242 +17,144 @@ const placarMaq = document.querySelector("#playerComputador");
 
 //Eventos De Click
 pedra.addEventListener("click", () => {
-    iconePlay.src = "img/carvao.png";
-    playerJogo = "pedra";
-    start();
+  iconePlay.src = "img/carvao.png";
+  playerJogo = "pedra";
+  start();
 });
 
 papel.addEventListener("click", () => {
-    iconePlay.src = "img/papel-toalha.png";
-    playerJogo = "papel";
-    start();
+  iconePlay.src = "img/papel-toalha.png";
+  playerJogo = "papel";
+  start();
 });
 
 tesoura.addEventListener("click", () => {
-    iconePlay.src = "img/tesoura.png";
-    playerJogo = "tesoura";
-    start();
+  iconePlay.src = "img/tesoura.png";
+  playerJogo = "tesoura";
+  start();
 });
-
-
-
-//Recebe a escolha do Player
-//var playerJogo = window.prompt("Escolha entre Pedra Papel ou Tesoura");
 
 //Contador referente o Placar
 var contadorEmpate = 0;
 var contadorPlayer = 0;
 var contadorMaquina = 0;
 
-//Start do jogo
-function start() {
-
-    //Dados = escolha da maquina
-    var dados = getComputerChoice();
-
-    //Inicia o jogo
-    playRound(dados);
-}
-
 //Função gerar um nome aleatorio "Pedra", "Papel" ou "Tesoura"
 function getComputerChoice() {
+  //Array de nomes
+  const jogo = ["pedra", "papel", "tesoura"];
 
-    //Array de nomes
-    const jogo = ["pedra", "papel", "tesoura"];
+  //Gera um indice aleatorio
+  var indice = Math.floor(Math.random() * jogo.length);
 
-    //Gera um indice aleatorio
-    var indice = Math.floor(Math.random() * jogo.length);
-
-    //Retorna o nome aleatorio
-    return jogo[indice];
+  //Retorna o nome aleatorio
+  return jogo[indice];
 }
 
-function playRound(dados) {
+//Start do jogo
+function start() {
+  //Dados = escolha da maquina
+  var dados = getComputerChoice();
 
-    //Pega o nome aleatorio
-    let computerSelection = dados;
-    console.log(`Escolha da maquina: ${computerSelection}`);
-
-    //Altera o icone de acordo a escolha da Maquina na tela
-    switch (computerSelection) {
-        case "pedra":
-            iconeMaq.src = "img/carvao.png";
-            break;
-
-        case "papel":
-            iconeMaq.src = "img/papel-toalha.png";
-            break;
-
-        case "tesoura":
-            iconeMaq.src = "img/tesoura.png";
-            break;
-    
-        default:
-            break;
-    }
-
-
-    //Escolha do Player
-    let playerSelection = playerJogo;
-    console.log(`Escolha do Player: ${playerSelection}`);
-
-    //Comparar o resultado 
-    if (computerSelection == playerSelection) {
-
-        statusText.innerHTML = "Empate!!"
-
-        /*
-        console.log(`Computer: ${computerSelection}, Player: ${playerSelection}`);
-        console.log("Empate!!");
-        contadorEmpate++
-        */
-
-    } 
-
-    if (playerSelection == "pedra" && computerSelection == "tesoura") {
-
-        //Resultado da rodada
-        statusText.innerHTML = "Você Ganhou!!"
-
-        //Altera o contador
-        contadorPlayer++
-
-        //Exibe o placar
-        placarPlay.innerHTML = contadorPlayer
-
-        /*
-        console.log(`Resultado: ${playerSelection} ganha de ${computerSelection}`);
-        console.log("Player Ganhou!!");
-        contadorPlayer++
-        */
-
-    } else if (playerSelection == "pedra" && computerSelection == "papel") {
-
-        statusText.innerHTML = "Maquina Ganhou!!"
-        contadorMaquina++
-        placarMaq.innerHTML = contadorMaquina
-
-        /*
-        console.log(`Resultado: ${playerSelection} perdeu para ${computerSelection}`);
-        console.log("Maquina Ganhou!!");
-        contadorMaquina++
-        */
-
-    }
-
-    if (playerSelection == "tesoura" && computerSelection == "papel") {
-
-        statusText.innerHTML = "Você Ganhou!!"
-        contadorPlayer++
-        placarPlay.innerHTML = contadorPlayer
-
-        /*
-        console.log(`Resultado: ${playerSelection} ganha de ${computerSelection}`);
-        console.log("Player Ganhou!!");
-        contadorPlayer++
-        */
-
-    } else if (playerSelection == "tesoura" && computerSelection == "pedra") {
-
-        statusText.innerHTML = "Maquina Ganhou!!"
-        contadorMaquina++
-        placarMaq.innerHTML = contadorMaquina
-
-        /*
-        console.log(`Resultado: ${playerSelection} perdeu para ${computerSelection}`);
-        console.log("Maquina Ganhou!!");
-        contadorMaquina++
-        */
-
-    }
-
-    if (playerSelection == "papel" && computerSelection == "pedra") {
-
-        statusText.innerHTML = "Você Ganhou!!"
-        contadorPlayer++
-        placarPlay.innerHTML = contadorPlayer
-
-        /*
-        console.log(`Resultado: ${playerSelection} ganha de ${computerSelection}`);
-        console.log("Player Ganhou!!");
-        contadorPlayer++
-        */
-
-    } else if (playerSelection == "papel" && computerSelection == "tesoura") {
-
-        statusText.innerHTML = "Maquina Ganhou!!"
-        contadorMaquina++
-        placarMaq.innerHTML = contadorMaquina
-
-        /*
-        console.log(`Resultado: ${playerSelection} perdeu para ${computerSelection}`);
-        console.log("Maquina Ganhou!!");
-        contadorMaquina++
-        */
-
-    }
-
-    verificar();
+  //Inicia o jogo
+  alterImage(dados);
 }
 
-//Responsavel por verificar o placar
-function verificar() {
-    
-    //Quando for igual a 5
-    if (contadorPlayer == 5) {
+//Alteração dos icones
+function alterImage(dados) {
+  //Pega o nome aleatorio
+  let computerSelection = dados;
 
-        //Exibe o alerta
-        alert("Você ganhou da maquina Parabens!!");
+  console.log(`Escolha da maquina: ${computerSelection}`);
 
-        //Reseta o contator
-        contadorMaquina = 0;
-        contadorPlayer = 0;
+  //Altera o icone de acordo a escolha da Maquina na tela
+  switch (computerSelection) {
+    case "pedra":
+      iconeMaq.src = "img/carvao.png";
+      break;
 
-        //Reseta o placar
-        placarPlay.innerHTML = 0;
-        placarMaq.innerHTML = 0;
-    }
+    case "papel":
+      iconeMaq.src = "img/papel-toalha.png";
+      break;
 
-    if (contadorMaquina == 5) {
-        alert("Putz não foi dessa vez");
-        contadorMaquina = 0;
-        contadorPlayer = 0;
+    case "tesoura":
+      iconeMaq.src = "img/tesoura.png";
+      break;
 
-        placarPlay.innerHTML = 0;
-        placarMaq.innerHTML = 0;
-    }
-
-    console.log(`Contagem de Empate: ${contadorEmpate}`);
-    console.log(`Contagem de Vitorias Player: ${contadorPlayer}`);
-    console.log(`Contagem de Vitorias Maquina: ${contadorMaquina}`);
-    
-}
-
-/*
-
-//Executa o Jogo 5 vezes
-function game() {
-
-    /*
-    //Estrutura de Repetição
-    for (let index = 0; index < 5; index++) {
-
-        //Chama a função responsavel pelo jogo
-        console.log(playRound());
-    }
-
-    //Exibi o Placar
-    console.log(`Contagem de Empate: ${contadorEmpate}`);
-    console.log(`Contagem de Vitorias Player: ${contadorPlayer}`);
-    console.log(`Contagem de Vitorias Maquina: ${contadorMaquina}`);
-
-}
-
-function nomeAleatorio(nomes) {
-    var indice = Math.floor(Math.random() * nomes.length);
-    return nomes[indice];
+    default:
+      break;
   }
 
+  playRound(computerSelection);
+}
 
-console.log(nome); // exibe um nome aleatório da lista
-*/
+//Testa os resultados
+function playRound(computerSelection) {
+  //Escolha do Player
+  let playerSelection = playerJogo;
+  console.log(`Escolha do Player: ${playerSelection}`);
+
+  //Comparar o resultado
+  if (computerSelection == playerSelection) {
+
+    statusText.innerHTML = "Empate !!";
+
+  } else if (playerSelection == "pedra" && computerSelection == "tesoura") {
+
+    contadorPlayer++;
+    placarPlay.innerHTML = contadorPlayer;
+    statusText.innerHTML = "Vitoria na Rodada";
+
+  } else if (playerSelection == "pedra" && computerSelection == "papel") {
+
+    contadorMaquina++;
+    placarMaq.innerHTML = contadorMaquina;
+    statusText.innerHTML = "Derrota na Rodada";
+
+  } else if (playerSelection == "tesoura" && computerSelection == "papel") {
+
+    contadorPlayer++;
+    placarPlay.innerHTML = contadorPlayer;
+    statusText.innerHTML = "Vitoria na Rodada";
+
+  } else if (playerSelection == "tesoura" && computerSelection == "pedra") {
+
+    contadorMaquina++;
+    placarMaq.innerHTML = contadorMaquina;
+    statusText.innerHTML = "Derrota na Rodada";
+
+  } else if (playerSelection == "papel" && computerSelection == "pedra") {
+
+    contadorPlayer++;
+    placarPlay.innerHTML = contadorPlayer;
+    statusText.innerHTML = "Vitoria na Rodada";
+
+  } else if (playerSelection == "papel" && computerSelection == "tesoura") {
+
+    contadorMaquina++;
+    placarMaq.innerHTML = contadorMaquina;
+    statusText.innerHTML = "Derrota na Rodada";
+  }
+
+  // Resultado Final
+  if (contadorPlayer == 5) {
+    //Reseta o contator
+    contadorMaquina = 0;
+    contadorPlayer = 0;
+
+    //Reseta o placar
+    placarPlay.innerHTML = 0;
+    placarMaq.innerHTML = 0;
+
+    alert("Parabéns. Você foi vitorioso!!");
+    
+  } else if (contadorMaquina == 5) {
+    contadorMaquina = 0;
+    contadorPlayer = 0;
+
+    placarPlay.innerHTML = 0;
+    placarMaq.innerHTML = 0;
+
+    alert("Não foi dessa vez. Boa sorte na próxima.");
+  }
+}
